c755bab616629c847633f2536a8f9fba57e0f06f
Maikell Oliveira dos Santos <maikell.ods10@gmail.com>
2026-05-03 13:39:07 -0400

    fix login

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 const bcrypt = require("bcryptjs");
 const pool = require("../db");
+const jwt = require("jsonwebtoken");
 
 const register = async (req, res) => {
     const { name, email, password } = req.body;
